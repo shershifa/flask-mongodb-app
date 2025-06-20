ff485955791a958f8ba361c9bdc626e22d9432f4
sherkhan131996@gmail.com <sherkhan131996@gmail.com>
2025-06-20 16:15:49 +0530

    Add /submittodoitem backend route in master_2

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, jsonify
 import json
 import os
+from flask import Flask, request, redirect
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to local or cloud MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["todoDB"]
+collection = db["items"]
+
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo():
+    item_name = request.form.get("itemName")
+    item_description = request.form.get("itemDescription")
+
+    if item_name and item_description:
+        collection.insert_one({
+            "itemName": item_name,
+            "itemDescription": item_description
+        })
+
+    return redirect("/todo")
 
 app = Flask(__name__)
 
